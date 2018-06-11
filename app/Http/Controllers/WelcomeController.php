110e8793a471978c108501255934ec359f934893
Jhondooo <dooo.inc@gmail.com>
2018-06-11 23:22:34 +0000

    want

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -16,9 +16,11 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        $items = Item::orderBy('updated_at', 'desc')->paginate(20);
+        return view('welcome', [
+            'items' => $items,
+        ]);
     }
-
     /**
      * Show the form for creating a new resource.
      *
